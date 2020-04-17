6caf3f7aff73e2337ebe8201b6cf44a956ef2000
lori <lori@labellacreative.com>
2020-04-17 17:22:52 -0400

    added more styles to contact page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -33,3 +33,16 @@ function create_custom_post_types() {
     );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+//Contact Us Page body_class
+add_filter( 'body_class','accelerate_child_body_classes' );
+function accelerate_child_body_classes( $classes ) {
+
+
+if (is_page('contact-us') ) {
+$classes[] = 'contact-us';
+}
+
+return $classes;
+
+}
