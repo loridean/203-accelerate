e0653c95a42682c1ffee347f5aa26018c73ffd36
lori <lori@labellacreative.com>
2020-04-23 15:15:45 -0400

    Added advanced custom fields service items to the About page and styled page. Updated  on single case studies.

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -27,7 +27,37 @@ get_header(); ?>
 			<?php the_content(); ?>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .main-content -->
-
 </div><!-- #primary -->
+
+<section class="services-intro">
+	<div class="site-content">
+		<h4>our services</h4>
+		<p>We take pride in our clients and the content we create for them.
+Here’s a brief overview of our offered services.</p>
+	</div>
+</section>
+
+
+<section class="services-list site-content">
+	<?php query_posts('post_type=about'); ?>
+		<?php while ( have_posts() ) : the_post();
+			$icon = get_field("icon");
+			$size = "custom";
+		?>
+		
+	<div class="services-item">
+		<div class="services-image">
+				<?php echo wp_get_attachment_image($icon, $size); ?>
+		</div>
+		<div class="services-info">
+				<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+		</div>
+	</div>
+
+			<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+
+</section>
 
 <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -20,7 +20,7 @@ get_header(); ?>
 		$image_1 = get_field('image_1');
 		$image_2 = get_field('image_2');
 		$image_3 = get_field('image_3');
-		$size = 'custom' ?>
+		$size = "full" ?>
 
 			<article class="case-study">
 				<aside class="case-study-sidebar">
